b403d6cfd5292017c846f0c04cbd51aef492d025
Home <aishamirza0002@gmail.com>
2024-01-02 22:19:32 +0530

    fix:projects section project card nav width contact section margin top bottom

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -14,10 +14,36 @@ export default function Project() {
           Currently working on this section (in production )
         </h2>
         <div className={styles.projectsCardContainer}>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          <ProjectCard
+            id={"01."}
+            header="MovieApp"
+            info="a web application about movies that lets you search movies get inso about them, and rate them, keep a check of which you have seen."
+          />
+          <ProjectCard
+            id={"02."}
+            header="TechBlog"
+            info="an initiative by me for me to learn more about frontend development that includes articles and the problems I have faced during project building."
+          />
+          <ProjectCard
+            id={"03."}
+            header="Eat-n-Split"
+            info="get your bills sorted with this simple web app and split bill easily keep note of who owes you and whom you owe."
+          />
+          <ProjectCard
+            id={"04."}
+            header="QuizzApp"
+            info="simple react quizz made with react"
+          />
+          <ProjectCard
+            id={"05."}
+            header="floraPhotos"
+            info="a beautiful website with beautiful animations."
+          />
+          <ProjectCard
+            id={"06."}
+            header="Worldwise "
+            info="a navigation web app so that you can know where you have been and where to go next."
+          />
         </div>
       </div>
     </section>
--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -1,11 +1,46 @@
 import styles from "./ProjectCard.module.css";
-export default function ProjectCard() {
+import { motion } from "framer-motion";
+export default function ProjectCard({ header, id, info }) {
   return (
-    <div className={styles.cardContainer}>
-      <figure>
-        <img src="https://picsum.photos/id/287/250/300" alt="Mountains" />
-        <figcaption></figcaption>
-      </figure>
-    </div>
+    <motion.div
+      className={styles.cardContainer}
+      initial="initial"
+      whileHover="hover"
+    >
+      <img
+        src="https://static.thenounproject.com/png/4996895-200.png"
+        alt={``}
+      />
+      <div>
+        <motion.div
+          variants={{
+            initial: { y: 150 },
+            hover: { y: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+          className={styles.cardInfoContainer}
+        >
+          <motion.span
+            variants={{
+              initial: { opacity: 1 },
+              hover: { opacity: 0 },
+            }}
+            className={styles.cardId}
+          >
+            {id}
+          </motion.span>
+          <p className={styles.cardHeader}>{header}</p>
+          <motion.p
+            variants={{
+              initial: { opacity: 0 },
+              hover: { opacity: 1 },
+            }}
+            className={styles.cardinfo}
+          >
+            {info}
+          </motion.p>
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
