043dd1f8c88554913632a1afcefd874c1115a624
Home <aishamirza0002@gmail.com>
2024-02-16 21:49:06 +0530

    added links

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -7,7 +7,7 @@ export default function Project() {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({ target: ref });
   const x = useTransform(scrollYProgress, [0, 1], ["5%", "-83.5%"]);
-  console.log(scrollYProgress);
+
   return (
     <section id="projects" className={styles.projects}>
       <div className={styles.content}>
@@ -19,38 +19,49 @@ export default function Project() {
         <div ref={ref} className={styles.horizontalScrollSection}>
           <div className={styles.projectCardsContainer}>
             <motion.div
+              transition={{
+                ease: "linear",
+                duration: 2,
+                x: { duration: 1 },
+              }}
               style={{ x }}
               className={styles.ProjectCardInnerContainer}
             >
               <ProjectCard
-                id={"01."}
-                header="MovieApp"
-                info="a web application about movies that lets you search movies get inso about them, and rate them, keep a check of which you have seen."
-              />
-              <ProjectCard
                 id={"02."}
                 header="TechBlog"
+                link="https://eclectic-cucurucho-0fe9b3.netlify.app"
                 info="an initiative by me for me to learn more about frontend development that includes articles and the problems I have faced during project building."
               />
               <ProjectCard
                 id={"03."}
                 header="Eat-n-Split"
+                link="https://aishamirza1.github.io/Eat-n-Split/"
                 info="get your bills sorted with this simple web app and split bill easily keep note of who owes you and whom you owe."
               />
               <ProjectCard
                 id={"04."}
                 header="QuizzApp"
+                link="https://aishamirza1.github.io/QuizzApp/"
                 info="simple react quizz made with react"
               />
               <ProjectCard
                 id={"05."}
                 header="floraPhotos"
+                link="https://aishamirza1.github.io/floraPhotos/"
                 info="a beautiful website with beautiful animations."
               />
               <ProjectCard
                 id={"06."}
                 header="Worldwise "
+                link=""
                 info="a navigation web app so that you can know where you have been and where to go next."
+              />
+              <ProjectCard
+                id={"01."}
+                header="MovieApp"
+                link="https://transcendent-daifuku-fc2ebf.netlify.app"
+                info="a web application about movies that lets you search movies get inso about them, and rate them, keep a check of which you have seen."
               />
             </motion.div>
           </div>
--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -1,46 +1,48 @@
 import styles from "./ProjectCard.module.css";
 import { motion } from "framer-motion";
-export default function ProjectCard({ header, id, info }) {
+export default function ProjectCard({ header, id, info, link }) {
   return (
-    <motion.div
-      className={styles.cardContainer}
-      initial="initial"
-      whileHover="hover"
-    >
-      <img
-        src="https://static.thenounproject.com/png/4996895-200.png"
-        alt={``}
-      />
-      <div>
-        <motion.div
-          variants={{
-            initial: { y: 150 },
-            hover: { y: 0 },
-          }}
-          transition={{ duration: 0.2 }}
-          className={styles.cardInfoContainer}
-        >
-          <motion.span
+    <a href={link} target="blank">
+      <motion.div
+        className={styles.cardContainer}
+        initial="initial"
+        whileHover="hover"
+      >
+        <img
+          src="https://static.thenounproject.com/png/4996895-200.png"
+          alt={``}
+        />
+        <div>
+          <motion.div
             variants={{
-              initial: { opacity: 1 },
-              hover: { opacity: 0 },
+              initial: { y: 150 },
+              hover: { y: 0 },
             }}
-            className={styles.cardId}
+            transition={{ duration: 0.2 }}
+            className={styles.cardInfoContainer}
           >
-            {id}
-          </motion.span>
-          <p className={styles.cardHeader}>{header}</p>
-          <motion.p
-            variants={{
-              initial: { opacity: 0 },
-              hover: { opacity: 1 },
-            }}
-            className={styles.cardinfo}
-          >
-            {info}
-          </motion.p>
-        </motion.div>
-      </div>
-    </motion.div>
+            <motion.span
+              variants={{
+                initial: { opacity: 1 },
+                hover: { opacity: 0 },
+              }}
+              className={styles.cardId}
+            >
+              {id}
+            </motion.span>
+            <p className={styles.cardHeader}>{header}</p>
+            <motion.p
+              variants={{
+                initial: { opacity: 0 },
+                hover: { opacity: 1 },
+              }}
+              className={styles.cardinfo}
+            >
+              {info}
+            </motion.p>
+          </motion.div>
+        </div>
+      </motion.div>
+    </a>
   );
 }
